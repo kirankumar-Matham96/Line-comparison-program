a08457265afe20321ccbd35478567511e2ce44e5
kirankumar-Matham96 <mathamkirankumar96@gmail.com>
2021-03-22 18:29:42 +0530

    ADD: Checkig if the lines are equal or not

--- lineComparisonProgram.java
+++ lineComparisonProgram.java
@@ -3,36 +3,76 @@ public class lineComparisonProgram
 {
 	public static void main(String[] args)
 	{
+		Integer X1,X2,x1,x2,Y1,Y2,y1,y2;
 		//(x2-x1)^2+(y2-y1)^2
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter X1 co-ordinate: ");
-		int X1 = sc.nextInt();
+		X1 = sc.nextInt();
 		System.out.println("Enter Y1 co-ordinate: ");
-		int Y1 = sc.nextInt();
+		Y1 = sc.nextInt();
 		System.out.println("Enter X2 co-ordinate: ");
-		int X2 = sc.nextInt();
+		X2 = sc.nextInt();
 		System.out.println("Enter Y2 co-ordinate: ");
-		int Y2 = sc.nextInt();
-		double length_of_line = 0;
+		Y2 = sc.nextInt();
+
+		System.out.println("Enter x1 co-ordinate: ");
+		x1 = sc.nextInt();
+		System.out.println("Enter y1 co-ordinate: ");
+		y1 = sc.nextInt();
+		System.out.println("Enter x2 co-ordinate: ");
+		x2 = sc.nextInt();
+		System.out.println("Enter y2 co-ordinate: ");
+		y2 = sc.nextInt();
+
+		double length_of_line_1 = 0;
+		double length_of_line_2 = 0;
 
 		if(X2 > X1 && Y2 > Y1) //For keeping the value positive inside 'sqrt' method
 		{
-			length_of_line = Math.sqrt((X2-X1)*(X2-X1)+(Y2-Y1)*(Y2-Y1));
+			length_of_line_1 = Math.sqrt((X2-X1)*(X2-X1)+(Y2-Y1)*(Y2-Y1));
 		}
 		if(X1 > X2 && Y1 > Y2)
 		{
-			length_of_line = Math.sqrt((X1-X2)*(X1-X2)+(Y1-Y2)*(Y1-Y2));
+			length_of_line_1 = Math.sqrt((X1-X2)*(X1-X2)+(Y1-Y2)*(Y1-Y2));
 		}
 		if(X2 > X1 && Y1 > Y2)
 		{
-			length_of_line = Math.sqrt((X2-X1)*(X2-X1)+(Y1-Y2)*(Y1-Y2));
+			length_of_line_1 = Math.sqrt((X2-X1)*(X2-X1)+(Y1-Y2)*(Y1-Y2));
 		}
 		else
 		{
-			length_of_line = Math.sqrt((X2-X1)*(X2-X1)+(Y2-Y1)*(Y2-Y1));
+			length_of_line_1 = Math.sqrt((X2-X1)*(X2-X1)+(Y2-Y1)*(Y2-Y1));
 		}
 
-		System.out.println("length: "+length_of_line);
 
+		if(x2 > x1 && y2 > y1)
+		{
+			length_of_line_2 = Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+		}
+		if(x1 > x2 && y1 > y2)
+		{
+			length_of_line_2 = Math.sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2));
+		}
+		if(x2 > x1 && y1 > y2)
+		{
+			length_of_line_2 = Math.sqrt((x2-x1)*(x2-x1)+(y1-y2)*(y1-y2));
+		}
+		else
+		{
+			length_of_line_2 = Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+		}
+
+
+		Double dl1 = length_of_line_1;
+		Double dl2 = length_of_line_2;
+
+		if(dl1.equals(dl2))
+		{
+			System.out.println("Two lines are equal in length");
+		}
+		else
+		{
+			System.out.println("Two lines are NOT equal in length");
+		}
 	}
 }
